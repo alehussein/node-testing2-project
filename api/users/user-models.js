609411981ea31040ed5df3eea39298863a770aa6
Alejandro Hussein <alejandrohussein1@gmail.com>
2023-06-09 18:19:25 -0500

    wip

--- api/users/user-models.js
+++ api/users/user-models.js
@@ -8,7 +8,13 @@ function getById(id){
     return db('users').where('id', id).first()
 }
 
+async function add(user){
+    return  await db('users').insert(user).then(([id]) =>{
+        return db('users').where('id', id).first()
+    })
+}
 module.exports = {
     getAll,
-    getById
+    getById,
+    add
 }
